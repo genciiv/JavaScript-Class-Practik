da367f64375c71f12e056e185a45285e21891c9d
G-code <30412971+genciiv@users.noreply.github.com>
2022-09-13 18:55:40 +0200

    Add files via upload

---
+++ JavaScript Class Practik/Part_12 Type Conversion/script.js
@@ -0,0 +1,9 @@
+// Lloji i konvertimit = ndryshimi i llojit te te dhenes ne nje te dhene tjeter (strings, numbers, boolean)
+
+let age = window.prompt("Sa vjec jeni ju ?");
+
+console.log(typeof age);
+age = Number(age);
+age += 1;
+
+console.log("Gezuar ditelindjen !", age, "Vjec");
