2281690df87e658934b8916ae3f2cab7041f9da8
G-code <30412971+genciiv@users.noreply.github.com>
2022-09-13 19:36:05 +0200

    Add files via upload

---
+++ JavaScript Class Practik/Part_14 String Methods/script.js
@@ -0,0 +1,12 @@
+// properties dhe metodat e Strings
+
+let emri = "Genci Code";
+
+console.log(emri.length);
+console.log(emri.charAt(0));
+console.log(emri.indexOf("e"));
+console.log(emri.lastIndexOf("e"));
+
+// emri = emri.trim();
+//  emri = emri.toUpperCase();
+// emri = emri.toLowerCase();
