92055391a2ef46496cd1db2c626582ab0874bd14
G-code <30412971+genciiv@users.noreply.github.com>
2022-09-13 15:19:21 +0200

    Add files via upload

---
+++ JavaScript Class Practik/Part_11 Strict Equality Operator/script.js
@@ -0,0 +1,10 @@
+// = operatori i barazimit
+// ==  operatori i krahasimit
+// === operatori i barazimit
+
+let x=3.14;
+if(x===3.14){
+    console.log("Kjo eshte Pi");
+}else{
+    console.log("Kjo nuk eshte pi");
+}
