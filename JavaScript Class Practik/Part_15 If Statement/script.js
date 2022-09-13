8efc56f2852080d79e89d12daa5352861ec30863
G-code <30412971+genciiv@users.noreply.github.com>
2022-09-13 21:17:03 +0200

    Add files via upload

---
+++ JavaScript Class Practik/Part_15 If Statement/script.js
@@ -0,0 +1,13 @@
+// if statement = nje form bazike per marrjen e vendimeve nqs nje kusht eshte true atehere bej dicka nqs nuk eshte ateher mos bej ate
+
+let mosha = 12;
+
+if(mosha>=18){
+    console.log("Ju je nje adoleshent");
+}
+else if(mosha<0){
+    console.log("Ju nuk keni lindur akoma!");
+}
+else{
+    console.log("Ju jeni nje femi !");
+}
---
+++ JavaScript Class Practik/Part_15 If Statement/script1.js
@@ -0,0 +1,8 @@
+let online = true;
+
+if(online){
+    console.log("Ju jeni online");
+}
+else{
+    console.log("Ju jeni offline");
+}
