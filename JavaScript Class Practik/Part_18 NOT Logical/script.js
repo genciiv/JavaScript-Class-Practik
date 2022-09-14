62c67bf352c68455dd418a2e9205f82f5a754bea
G-code <30412971+genciiv@users.noreply.github.com>
2022-09-14 08:45:37 +0200

    Add files via upload

---
+++ JavaScript Class Practik/Part_18 NOT Logical/script.js
@@ -0,0 +1,20 @@
+// Not logical operator zakonisht perdoret per te rezervuar nje kusht boolean vlere dhe 
+// true -> false     false -> true
+
+
+let temp = 15;
+let dielli = false;
+
+
+if(!(temp>0)){
+    console.log('perjashta eshte ftohte');
+}else{
+    console.log('eshe vape perjashta');
+}
+
+if(!dielli){
+    console.log('eshte mjergull perjashta');
+}
+else{
+    console.log('eshte diell perjashta');
+}
